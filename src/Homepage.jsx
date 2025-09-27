17de50ea9770061308c32be28acf936de8cbbdf3
Ami0480 <amifuku80@gmail.com>
2025-09-27 23:02:03 +0800

    Created basic html

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function Homepage() {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city.."
+          className="bg-gray-200"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <h1>Perth</h1>
+      <p>Monday Sep 27 | CLOUDY</p>
+      <p>Humidity: 48%, Wind: 7km/h</p>
+      <h2>20°C</h2>
+      <img src="images/cloudy.png" alt="current icon" className="w-24 h-24" />
+    </div>
+  );
 }
